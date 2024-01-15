f687fa042848b2dc6954a8cc21c9b6a89c26e048
Raza Fatma <126192356+RazaFatma@users.noreply.github.com>
2024-01-15 19:47:31 +0530

    Add files via upload
    initial.commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let searchBar = document.querySelector(".search-bar")
+let btn = document.querySelector(".submit")
+let inputValue = document.querySelector(".cityname")
+let temp = document.querySelector(".temp")
+let des = document.querySelector(".description") 
+let humidity = document.querySelector(".humidity")
+let wind = document.querySelector("wind")
+
+let apiKey = "baeaa9dd4ba7c67a06d7a1473c1dc31d"
+
